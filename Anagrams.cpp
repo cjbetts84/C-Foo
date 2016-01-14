3ceafae8084bb59fad29d92c15cc71349a6c40f5
C. Betts <cjbetts84@gmail.com>
2016-01-14 10:10:18 -0500

    Create Anagrams.cpp

---
+++ Anagrams.cpp
@@ -0,0 +1,20 @@
+/* Anagrams.cpp
+"Problem 7 - Anagrams"
+An anagram is a word or phrase that can be created from a second word 
+or phrase by rearranging the letters. For example STEP is an anagram 
+of PEST. Another way of stating this is that two phrases are anagrams 
+of one another if they consist of exactly the same letters while 
+ignoring spaces.
+
+Write a program which accepts two phrases and reports whether they are 
+anagrams. Your program needs to accept a pair of phrases and output a 
+message indicating if the words or phrases are anagrams. Upper and 
+lowercase letters are considered equivalent for this problem. Only 
+letters (A to Z) are considered in determining if two phrases are anagrams.
+
+Sample-
+Input / Input - Output
+HAT  /  Cat - Not anagrams
+dormitory / dirty room - anagrams
+POLL / Pool - not anagrams
+*/
