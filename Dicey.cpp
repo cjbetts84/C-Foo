58e278b84a8cd625ea612c713c2005d79fbb830b
C. Betts <cjbetts84@gmail.com>
2016-01-14 10:21:44 -0500

    Create Dicey.cpp

---
+++ Dicey.cpp
@@ -0,0 +1,30 @@
+/*Dicey.cpp
+"Problem 9 - Dicey"
+
+To make board games like Monopoly more interesting, we've invented
+many-sided dice. That is while a normal die can roll 1 through 6, 
+we've got dice that can roll 1 or 2, 1 through 3, 1 through 4, etc.
+(Ignore how we've managed to make such dice). As usual, the total of
+the dice is what counts. In addition, we always use three dice. We 
+only sell the dice in sets which have all the same number of sides. 
+So when games are played, all the dice have the same number of sides.
+Notice if we use three of the five-sided dice, the possible totals
+are 3, 4, 5, ..., 15.
+
+Write a program which when given the number of sides on the dice,
+gives the probability of each total as a fraction. 
+
+Sample-
+Input: The number of faces on each die? 4
+Output:
+Rolling a 3 has a probability of 1/64
+Rolling a 4 has a probability of 3/64
+Rolling a 5 has a probability of 6/64
+Rolling a 6 has a probability of 10/64
+Rolling a 7 has a probability of 12/64
+Rolling a 8 has a probability of 12/64
+Rolling a 9 has a probability of 10/64
+Rolling a 10 has a probability of 6/64
+Rolling a 11 has a probability of 3/64
+Rolling a 12 has a probability of 1/64
+*/
